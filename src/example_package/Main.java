3f079bf84ab294e79316d43507709a5025abdb70
Runnercramer <cristiandavidvargasmesa@gmail.com>
2023-05-19 08:34:55 -0500

    First commit

---
+++ src/example_package/Main.java
@@ -0,0 +1,15 @@
+package example_package;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private int age = 23;
+    private long id = 1023876543L;
+    private double price = 324.50;
+    private boolean value = true;
+    private String name = "Cristian";
+
+}
