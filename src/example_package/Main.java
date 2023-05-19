f7acf3a9233e03a459b5ba92f04d604fc39825c0
Runnercramer <cristiandavidvargasmesa@gmail.com>
2023-05-19 08:39:03 -0500

    Final commit

--- src/example_package/Main.java
+++ src/example_package/Main.java
@@ -3,13 +3,17 @@ package example_package;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(age);
+        System.out.println(id);
+        System.out.println(price);
+        System.out.println(value);
+        System.out.println(name);
     }
 
-    private int age = 23;
-    private long id = 1023876543L;
-    private double price = 324.50;
-    private boolean value = true;
-    private String name = "Cristian";
+    private static int age = 23;
+    private static long id = 1023876543L;
+    private static double price = 324.50;
+    private static boolean value = true;
+    private static String name = "Cristian";
 
 }
